d85a835298f52c34e4604e877473fa7b0302ddf0
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-06-19 10:41:41 +0800

    Add documentation

--- pool.go
+++ pool.go
@@ -4,11 +4,13 @@ import (
 	"sync"
 )
 
+// Pool is a generic implementation of the standard library's sync.Pool.
 type Pool[E any] struct {
 	rwmutex *sync.RWMutex
 	pool    *sync.Pool
 }
 
+// NewPool returns a new Pool with generator new.
 func NewPool[E any](new func() E) *Pool[E] {
 	pool := &Pool[E]{
 		rwmutex: &sync.RWMutex{},
@@ -20,16 +22,25 @@ func NewPool[E any](new func() E) *Pool[E] {
 	return pool
 }
 
+// Get selects an arbitrary item from the Pool, removes it from the Pool, and
+// returns it to the caller. Get may choose to ignore the pool and treat it as
+// empty. Callers should not assume any relation between values passed to Put
+// and the values returned by Get.
+//
+// If Get would otherwise return nil and the generator is non-nil, Get returns
+// the result of calling the generator.
 func (pool *Pool[E]) Get() E {
 	pool.rwmutex.RLock()
 	defer pool.rwmutex.RUnlock()
 	return pool.pool.Get().(E)
 }
 
+// Put adds x to the pool.
 func (pool *Pool[E]) Put(x E) {
 	pool.pool.Put(x)
 }
 
+// Update updates the pool's generator to new.
 func (pool *Pool[E]) Update(new func() E) {
 	pool.rwmutex.Lock()
 	defer pool.rwmutex.Unlock()
